ead8646d2e61dcc377edbd3f7d18794560604d82
BoboBogus <75058008+BoboBogus@users.noreply.github.com>
2023-04-02 00:36:45 -0400

    multi bind

---
+++ inputrebind/src/RebindSet.java
@@ -0,0 +1,13 @@
+
+
+public class RebindSet {
+    public int index;
+    public int Bind;
+    public int Replace;
+    public int isKey;
+    public RebindSet(){
+        Bind = -1;
+        Replace = -1;
+        isKey = -1;
+    }
+}
